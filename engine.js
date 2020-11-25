574daf9ea3399df5a4ad9a245081a8943c8a06d1
Glitch (hello-express) <none>
2020-11-25 02:24:59 +0000

    👞🐁 Checkpoint ./engine.js:8045597/1366 ./sites.js:8045597/27

--- engine.js
+++ engine.js
@@ -1,17 +1,28 @@
-var urls = Object.keys(require("./sites.js"))
-var config = require("./config.js")
-var sites = require("./sites.js")
-var request = require('sync-request');
-const fs = require("fs")
-var i = 0
+var urls = Object.keys(require("./sites.js"));
+var config = require("./config.js");
+var sites = require("./sites.js");
+var request = require("sync-request");
+const fs = require("fs");
+var i = 0;
 // Loop through all of the sites and get data
-while(i < urls.length){
+while (i < urls.length) {
   // Fetch site
-var data = request('GET', urls[i]).getBody().toString()
-// Parse data
-data = JSON.parse(data)
+  var data = request("GET", urls[i], {
+    headers: {
+      "user-agent": "example-user-agent"
+    }
+  })
+    .getBody()
+    .toString();
+  // Parse data
+  data = JSON.parse(data);
   // generate metatags
-  data = require("./generator/metatags.js")(data.title,data.description, `${config.hostname}/${sites[urls[i]]}`, data.image)
-  console.log(data)
-  i++
+  data = require("./generator/metatags.js")(
+    data.title || "",
+    data.description || "",
+    `${config.hostname}/${sites[urls[i]]}`,
+    data.image
+  );
+  console.log(data);
+  i++;
 }
--- sites.js
+++ sites.js
@@ -1,3 +1,4 @@
 module.exports = {
+  // "url": "slug"
   "https://slow-flying-reading.glitch.me/demo.json": "hello!"
 };
