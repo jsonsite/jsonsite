c80b16d9020a17ad366a58ad7b0acaaa0d2ad2db
Glitch (hello-express) <none>
2020-11-25 17:40:21 +0000

    🏵🏡 Checkpoint ./generator/pages.js:8045597/118 ./generator/logic.js:8045597/266

--- generator/logic.js
+++ generator/logic.js
@@ -8,7 +8,12 @@ module.exports = function() {
   div.innerHTML = '<h1 class="404-title">Whoops!</h1><br><p class="404-description">That page could not be found. Would you like to go back <a href="#main">home?</a></p>';
   document.body.appendChild(div);
   }
-  document.getElementById(location.hash.substr(1)).hidden = false}
+  var i = 0;
+  while(i < ids.length){
+   document.getElementById(ids[i]).setAttribute("style", "display:none;");
+   i++
+  }
+  document.getElementById(location.hash.substr(1)).setAttribute("style", "display:block;");}
   // Start loading on hash change and on load
   window.onhashchange = nav;
   window.addEventListener('DOMContentLoaded', (event) => {
--- generator/pages.js
+++ generator/pages.js
@@ -6,7 +6,7 @@ module.exports = function(page){
   var obj = [];
   while (i < page.length){
     obj.push(page[i].id)
-    html = html + `<div id="${page[i].id}" hidden="true" class="siimple-content siimple-content--extra-large">
+    html = html + `<div id="${page[i].id}" style="display:none;" class="siimple-content siimple-content--extra-large">
     <h3 class="siimple-h3 page-title">${page[i].title}</h3>
     <div class="siimple-p">${md.render(page[i].content)}</div>
     </div>
