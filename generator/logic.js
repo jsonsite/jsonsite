23432015ef0f02b1726029226522a8d0d5973941
Glitch (hello-express) <none>
2020-11-25 16:32:01 +0000

    💼🏟 Checkpoint ./generator/logic.js:8045597/223

--- generator/logic.js
+++ generator/logic.js
@@ -1,3 +1,4 @@
+// This is the main logic, it switches/displays pages and loads CSS
 module.exports = function() {
   return `<script>if(location.hash.substr(1) == ""){
    location.hash = "main"
@@ -7,6 +8,6 @@ module.exports = function() {
   div.innerHTML = '<h1 class="404-title">Whoops!</h1><br><p class="404-description">That page could not be found. Would you like to go back <a href="#main">home?</a></p>';
   document.body.appendChild(div);
   }
-  document.getElementById(location.hash.substr(1)).hidden = false</script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  document.getElementById(location.hash.substr(1)).hidden = false</script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/siimple@3.3.1/dist/siimple.min.css">
 `;
 };
