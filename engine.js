9120f4480b9d440eb50069517e9638ba76e5bf8e
Glitch (hello-express) <none>
2020-11-25 02:01:30 +0000

    🌝🎪 Checkpoint ./engine.js:8045597/541 ./sites.js:8045597/78 ./generator/metatags.js:8045597/2867

--- engine.js
+++ engine.js
@@ -1,8 +1,15 @@
+var urls = Object.keys(require("/sites.js"))
 var sites = require("/sites.js")
 var request = require('sync-request');
+const fs = require("fs")
 var i = 0
-var obj = []
-while(i < sites.length){
-
+// Loop through all of the sites and get data
+while(i < urls.length){
+  // Fetch site
+var data = request('GET', urls[i]).getBody().toString()
+// Parse data
+data = JSON.parse(data)
+  // generate metatags
+  data = require("./generator/metatags.js")(data.title,data.description, sites[urls[i]])
   i++
 }
---
+++ generator/metatags.js
@@ -0,0 +1,20 @@
+module.exports = function(title, desc, url, image) {
+  return `<!-- Generated with metatags.io! --><!-- Primary Meta Tags -->
+<title>${title}</title>
+<meta name="title" content="${title}">
+<meta name="description" content="${desc}">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="${url}">
+<meta property="og:title" content="${title}">
+<meta property="og:description" content="${desc}">
+<meta property="og:image" content="${image}">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="${url}">
+<meta property="twitter:title" content="${title}">
+<meta property="twitter:description" content="${desc}">
+<meta property="twitter:image" content="${image || ""}">`;
+};
