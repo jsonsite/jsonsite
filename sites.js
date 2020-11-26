518e9779dc9268aa02b66f0f153bcd5e8583e787
David <62346025+aboutDavid@users.noreply.github.com>
2020-11-26 18:20:39 -0500

    Add "david" from "david-jsonsite.glitch.me"

--- sites.js
+++ sites.js
@@ -1,4 +1,5 @@
 module.exports = {
   // "url": "slug"
-  "https://slow-flying-reading.glitch.me/demo.json": "demo"
+  "https://slow-flying-reading.glitch.me/demo.json": "demo",
+  "https://david-jsonsite.glitch.me/": "david"
 };
